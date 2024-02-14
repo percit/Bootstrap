2cad3f423793a68dd56fba214c1b6695423e3030
Marcin Zdunek <marcin.zdunek@siili.com>
2024-02-14 18:56:38 +0100

    Add setting up rofi

--- rofi-install.sh
+++ rofi-install.sh
@@ -8,3 +8,9 @@ chmod +x setup.sh
 ./setup.sh
 #change version to one I like type-1 style 5
 sed -i "s/theme='style-1'/theme='style-5'/" ~/.config/rofi/launchers/type-1/launcher.sh
+
+
+#add keybinding ctrl r to that app
+# ~/.config/rofi/launchers/type-1/launcher.sh to jest glowna apka
+# ~/.config/rofi/launchers/type-1/powermenu.sh
+# itd
