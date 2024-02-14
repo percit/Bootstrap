2c8b7f5c92f5670215d85d14373f33f8fba4d251
Marcin Zdunek <marcin.zdunek@siili.com>
2024-02-14 19:08:50 +0100

    Add setting up zsh

--- zsh-install.sh
+++ zsh-install.sh
@@ -3,3 +3,17 @@
 #pobierz zsh, ohmyzsh
 #ustaw configi, tutaj chyba wystarczy echo
 #ustaw zsh jako default 
+
+if command -v zsh &> /dev/null; then
+    echo "zsh is already installed."
+else
+    sudo apt install zsh
+fi
+
+zsh
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
+add ZSH_THEME="powerlevel10k/powerlevel10k" to ~/.zshrc pewnie przez exporta
+
+https://dev.to/abdfnx/oh-my-zsh-powerlevel10k-cool-terminal-1no0
+p10k configure trzeba odpalic i przeklikac
