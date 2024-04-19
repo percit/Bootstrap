a72a4b6684692bd60075090bc01d1ef4c1ce17e7
Marcin Zdunek <marcin.zdunek@siili.com>
2024-04-19 16:43:14 +0200

    Add gnome bedtime extension to after script

--- run_me_after_bootstrap.sh
+++ run_me_after_bootstrap.sh
@@ -4,14 +4,13 @@ echo "1. settings=>keyboard=>add a shortcut=> ctrl shift c => flameshot gui"
 echo -e "\n" 
 echo "2. settings=>keyboard=>add a shortcut=> ctrl r => ./.config/rofi/launchers/type-1/launcher.sh"
 echo -e "\n"
-echo "3. Run git config --global user.email "email"
-          git config --global user.name "name""
+echo "3. Run git config --global user.email email
+          git config --global user.name name"
 echo "4. Set settings => mouse and touchpad=> right click with click"
 echo -e "\n"
 echo "5. Download Notatki and set obsidian vault"
 echo -e "\n"
 echo "6. Change ZSH_THEME to powerlevel10k/powerlevel10k in  ~/.zshrc"
-echo "Add zsh-syntax-highlighting and zsh-autosuggestions to plugins in .zshrc and restart terminal"
 echo "Run 'source $ZSH/oh-my-zsh.sh' to configure zsh"`
 echo -e "\n"
 echo "7 download anki from herehttps://apps.ankiweb.net/"
@@ -20,4 +19,6 @@ echo "8 To set up Syncthing to launch in the launcher, click on 'Syncthing Web' 
 Then, select 'Show ID' on your phone, add the device by tapping the plus sign, and you can add files that way.
 Next, navigate to the folder on your phone within Syncthing and click on the username to sync. This will prompt a sync request on the web UI.
 If you still have problems use this: https://www.youtube.com/watch?v=8bHdcfVzrgk"
+echo -e "\n"
+echo "9 In chrome add gnome extension to chrome, then click that extension and install bedtime extension to have black-white screen"
 # echo -e "\n" is empty line
