e1cd2e0d5558f0edb54bc83164ca45293cb52b32
Marcin Zdunek <marcin.zdunek@siili.com>
2024-04-19 16:51:00 +0200

    Add espanso

--- run_me_after_bootstrap.sh
+++ run_me_after_bootstrap.sh
@@ -10,15 +10,33 @@ echo "4. Set settings => mouse and touchpad=> right click with click"
 echo -e "\n"
 echo "5. Download Notatki and set obsidian vault"
 echo -e "\n"
-echo "6. Change ZSH_THEME to powerlevel10k/powerlevel10k in  ~/.zshrc"
-echo "Run 'source $ZSH/oh-my-zsh.sh' to configure zsh"`
-echo -e "\n"
-echo "7 download anki from herehttps://apps.ankiweb.net/"
+echo "6. Run source $ZSH/oh-my-zsh.sh to configure zsh"
+echo "7 download anki from here https://apps.ankiweb.net/"
 echo -e "\n"
 echo "8 To set up Syncthing to launch in the launcher, click on 'Syncthing Web' in the actions menu at the top right corner.
 Then, select 'Show ID' on your phone, add the device by tapping the plus sign, and you can add files that way.
 Next, navigate to the folder on your phone within Syncthing and click on the username to sync. This will prompt a sync request on the web UI.
 If you still have problems use this: https://www.youtube.com/watch?v=8bHdcfVzrgk"
 echo -e "\n"
-echo "9 In chrome add gnome extension to chrome, then click that extension and install bedtime extension to have black-white screen"
+echo "9 In chrome add gnome extension to chrome, then click that extension and install bedtime extension to have black-white 
+screen"
+echo -e "\n"
+echo "10 Add this to /home/usr/.config/espanso/match/base.yml remembering that you need good indentation"
+printf '%s\n' '
+- trigger: ":a"
+  replace: "ä"
+- trigger: ":o"
+  replace: "ö"
+- trigger: ":u"
+  replace: "ü"
+- trigger: ":s"
+  replace: "ß"
+- trigger: ":A"
+  replace: "Ä"
+- trigger: ":O"
+  replace: "Ö"
+- trigger: ":U"
+  replace: "Ü"
+'
+
 # echo -e "\n" is empty line
---
+++ scripts/espanso-install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+wget https://github.com/federico-terzi/espanso/releases/download/v2.2.1/espanso-debian-x11-amd64.deb
+
+sudo apt install ./espanso-debian-x11-amd64.deb
+
+# Register espanso as a systemd service (required only once)
+espanso service register
+
+# Start espanso
+espanso start
